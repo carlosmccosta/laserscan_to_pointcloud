bd44b703c58b1aeebbef6b02f5834d028f80b34c
Carlos Costa <carloscosta.cmcc@gmail.com>
2015-06-13 17:08:43 +0100

    Changed extension of bash scripts from .sh to .bash

--- docs/eclipse_projects.bash
+++ docs/eclipse_projects.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "####################################################################################################"
 echo "##### Creating eclipse project for package laserscan_to_pointcloud"
@@ -10,7 +10,7 @@ cd ~/catkin_ws/build/laserscan_to_pointcloud
 cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 ~/catkin_ws/src/laserscan_to_pointcloud
 
 
-echo "\n\n"
+echo -e "\n\n"
 echo "----------------------------------------------------------------------------------------------------"
 echo ">>>>> Eclipse build  project in ~/catkin_ws/build/laserscan_to_pointcloud"
 echo ">>>>> Eclipse source project in ~/catkin_ws/src/laserscan_to_pointcloud"
@@ -18,7 +18,7 @@ echo "--------------------------------------------------------------------------
 
 
 
-echo "\n\n\n\n"
+echo -e "\n\n\n\n"
 echo "####################################################################################################"
 echo "##### Creating eclipse projects for catkin workspace"
 echo "####################################################################################################"
@@ -27,7 +27,7 @@ cd ~/catkin_ws
 catkin_make --force-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8
 
 
-echo "\n\n"
+echo -e "\n\n"
 echo "----------------------------------------------------------------------------------------------------"
 echo ">>>>> Eclipse catkin_ws project in ~/catkin_ws/build"
 echo "----------------------------------------------------------------------------------------------------"
